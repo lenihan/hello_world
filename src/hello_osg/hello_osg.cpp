548b7e281167be3e0de6237ad301357846628369
David Lenihan <davidlenihan@outlook.com>
2021-11-18 00:26:08 -0800

    osg building

---
+++ src/hello_osg/hello_osg.cpp
@@ -0,0 +1,8 @@
+#include <osgDB/ReadFile>
+#include <osgViewer/Viewer>
+int main(int argc, char** argv)
+{
+    osgViewer::Viewer viewer;
+    viewer.setSceneData(osgDB::readNodeFile("cessna.osg"));
+    return viewer.run();
+}
